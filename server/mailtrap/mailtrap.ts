647056f5703ebb73df3faf1460085e2407140e1a
ankit <ankitkarapurkar444@gmail.com>
2025-02-19 01:50:08 -0800

    mailtrap setup

---
+++ server/mailtrap/mailtrap.ts
@@ -0,0 +1,27 @@
+const { MailtrapClient } = require("mailtrap");
+import dotenv from "dotenv";
+dotenv.config();
+ 
+export const client = new MailtrapClient({
+  token:process.env.MAILTRAP_API_TOKEN!,
+});
+
+export const sender = {
+  email: "hello@demomailtrap.com",
+  name: "ankit",
+};
+const recipients = [
+  {
+    email: "ankitkarapurkar444@gmail.com",
+  }
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
